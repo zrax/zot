6ca2bcc0fe10ab1879a3a38e56bd1db56fabecf7
Michael Hansen <zrax0111@gmail.com>
2015-02-03 20:07:04 -0800

    Port to https://pypi.python.org/pypi/irc

--- zot.py
+++ zot.py
@@ -4,6 +4,9 @@ import os, sys
 import socket
 import re
 
+import irc.bot
+import irc.client
+
 IDENTIFIER = r'[A-Za-z_]\w*(?:(?:\.|->|::)[A-Za-z_]\w*)*'
 WSPACE = r'(?:\s|/\*(?:[^*]|\*[^/])*\*+/)'
 RE_PRE = re.compile(r'^%s*(\+\+|--)%s*(%s)(?:%s|;)*(?://.*)?$' % \
@@ -14,38 +17,24 @@ RE_QUERY = re.compile(r'^%s*\?%s*(%s)(?:%s|;)*(?://.*)?$' % \
                       (WSPACE, WSPACE, IDENTIFIER, WSPACE))
 RE_PART = re.compile(r'(\.|->|::)')
 
-ZOT_VERSION = "ZOT 0.3"
+ZOT_VERSION = "ZOT 0.4"
 DBASE_FILE = "zot.db"
 
-class zot:
+# Fix UTF-8 decoding errors
+irc.client.ServerConnection.buffer_class.errors = 'replace'
+
+class zot(irc.bot.SingleServerIRCBot):
     def __init__(self, addr, port, nick, channels):
-        self.server = (addr, port)
-        self.nick = nick
-        self.channels = channels
-
-        self.dbase = { }
-        self.load_dbase(DBASE_FILE)
-
-        self.sock = None
-        self.connect()
-
-    def connect(self):
-        print("Connecting...")
-        if self.sock:
-            self.sock.shutdown(socket.SHUT_RDWR)
-            self.sock.close()
-        self.sock = socket.create_connection(self.server, 600.0)
-        self.sock.send('USER %s . . %s\r\n' % (self.nick, self.nick))
-        self.sock.send('NICK %s\r\n' % self.nick)
-        for chan in self.channels:
-            self.sock.send('JOIN #%s\r\n' % chan)
+        irc.bot.SingleServerIRCBot.__init__(self, [(addr, port)], nick, nick)
+        self.channels_to_join = channels
 
     @staticmethod
     def normalize(ident):
         return RE_PART.sub('.', ident).lower()
 
-    def load_dbase(self, filename):
-        db = open(filename, 'r')
+    def load_dbase(self):
+        self.dbase = { }
+        db = open(DBASE_FILE, 'r')
         for line in db.readlines():
             try:
                 k, v = line.split(':', 1)
@@ -54,95 +43,11 @@ class zot:
                 pass
         db.close()
 
-    def save_dbase(self, filename):
-        db = open(filename, 'w')
+    def save_dbase(self):
+        db = open(DBASE_FILE, 'w')
         for k in self.dbase:
             db.write('%s:%s\n' % (k, str(self.dbase[k])))
         db.close()
-
-    def run(self):
-        packet = ''
-        bad_time = 0
-        while True:
-            try:
-                packet += self.sock.recv(4096)
-            except socket.timeout:
-                # Reconnect and try again
-                bad_time += 1
-                if bad_time > 3:
-                    print >>sys.stderr, "Having trouble staying connected...  Bailing out!"
-                    return
-                self.connect()
-                continue
-            if len(packet) == 0:
-                break
-
-            bad_time = 0
-            lines = packet.split('\r\n')
-            for ln in lines[:-1]:
-                self.parse(ln)
-
-            packet = lines[-1] if len(lines[-1]) else ''
-
-    def parse(self, line):
-        if len(line) == 0:
-            return
-
-        sender = ''
-        msg = line.split(None, 3)
-        if line[0] == ':':
-            sender = msg[0][1:]
-            msg = msg[1:]
-        if len(msg) < 1:
-            return
-
-        if msg[0] == 'PING':
-            # Lazy: Save the dbase on a timer!
-            self.sock.send('PONG ' + msg[1] + '\r\n')
-            self.save_dbase(DBASE_FILE)
-
-        elif msg[0] == 'PRIVMSG':
-            try:
-                recp = msg[1]
-                text = msg[2]
-            except IndexError:
-                print "Got bad message!"
-                return
-
-            snick = sender.split('!')[0]
-            if recp == self.nick:
-                dest = snick
-            else:
-                dest = recp
-
-            if text.startswith(':'):
-                text = text[1:]
-
-            match = RE_PRE.search(text)
-            if match is not None:
-                key = zot.normalize(match.group(2))
-                self.sendMsg(dest, self.doMath(key, match.group(1), snick))
-                return
-
-            match = RE_POST.search(text)
-            if match is not None:
-                key = zot.normalize(match.group(1))
-                self.sendMsg(dest, self.doMath(key, match.group(2), snick))
-                return
-
-            match = RE_QUERY.search(text)
-            if match is not None:
-                try:
-                    key = zot.normalize(match.group(1))
-                    value = self.dbase[key]
-                except KeyError:
-                    value = 0
-                self.sendMsg(dest, '%s = %d' % (key, value))
-                return
-
-            if text.lower() == '\x01version\x01':
-                self.sendNotice(snick, '\x01VERSION ' + ZOT_VERSION + '\x01')
-                return
 
     def update(self, key, delta):
         if key not in self.dbase:
@@ -150,7 +55,7 @@ class zot:
         else:
             self.dbase[key] += delta
 
-    def doMath(self, key, oper, snick):
+    def do_math(self, key, oper, snick):
         snick = zot.normalize(snick)
         if oper == '++':
             if key == snick:
@@ -168,11 +73,56 @@ class zot:
             self.update(key, -1)
         return '%s = %d' % (key, self.dbase[key])
 
-    def sendMsg(self, dest, msg):
-        self.sock.send('PRIVMSG ' + dest + ' :' + msg + '\r\n')
+    def parse(self, conn, event, is_privmsg):
+        snick = event.source.nick
+        text = event.arguments[0]
+        match = RE_PRE.search(text)
+        if match is not None:
+            key = zot.normalize(match.group(2))
+            conn.privmsg(event.target, self.do_math(key, match.group(1), snick))
+            return
 
-    def sendNotice(self, dest, msg):
-        self.sock.send('NOTICE ' + dest + ' :' + msg + '\r\n')
+        match = RE_POST.search(text)
+        if match is not None:
+            key = zot.normalize(match.group(1))
+            conn.privmsg(event.target, self.do_math(key, match.group(2), snick))
+            return
+
+        match = RE_QUERY.search(text)
+        if match is not None:
+            try:
+                key = zot.normalize(match.group(1))
+                value = self.dbase[key]
+            except KeyError:
+                value = 0
+            conn.privmsg(event.target, '%s = %d' % (key, value))
+            return
+
+
+    #
+    # IRC Bot events
+    #
+    def on_nicknameinuse(self, conn, event):
+        print >>sys.stderr, "Nick is already in use"
+        sys.exit(1)
+
+    def on_welcome(self, conn, event):
+        print >>sys.stderr, "Got connection, joining channels"
+        for chan in self.channels_to_join:
+            conn.join('#' + chan)
+
+    def on_privmsg(self, conn, event):
+        self.parse(conn, event, False)
+
+    def on_pubmsg(self, conn, event):
+        self.parse(conn, event, True)
+
+    def on_ctcp(self, conn, event):
+        snick = event.source.nick
+        if event.arguments[0] == 'VERSION':
+            conn.ctcp_reply(snick, 'VERSION ' + ZOT_VERSION)
+        else:
+            irc.bot.SingleServerIRCBot.on_ctcp(self, conn, event)
 
 
 if len(sys.argv) < 5:
@@ -183,5 +133,10 @@ host = sys.argv[1]
 port = int(sys.argv[2])
 nick = sys.argv[3]
 channels = sys.argv[4:]
+
 server = zot(host, port, nick, channels)
-server.run()
+server.load_dbase()
+try:
+    server.start()
+finally:
+    server.save_dbase()
